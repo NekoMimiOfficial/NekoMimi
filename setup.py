e5008903926e1abdf0d39b66611225a672fbcd92
NekoKitsune <mrcreaperwhantsadingdongtobedo@gmail.com>
2021-07-09 22:58:37 +0300

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="NekoMimi",
-    version="1.0.0",
+    version="1.0.1",
     author="NekoMimi",
     author_email="mrcreaperwhantsadingdongtobedo@gmail.com",
     description="A handy collection of tools",
