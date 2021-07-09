ba1d3a949bc47e3c0d5816918323ba41c1b32f90
NekoKitsune <mrcreaperwhantsadingdongtobedo@gmail.com>
2021-07-09 16:16:12 +0300

    new file:   pyproject.toml 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="NekoMimi",                     
-    version="1.0.0",                        
-    author="NekoMimi",                     
-    description="NekoMimi's special utilities",
-    long_description=long_description,      
+    name="NekoMimi",
+    version="1.0.0",
+    author="NekoMimi",
+    author_email="mrcreaperwhantsadingdongtobedo@gmail.com",
+    description="A handy collection of tools",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),    
+    url="https://github.com/NekoKitsune/NekoMimi",
+    project_urls={
+        "Bug Tracker": "https://github.com/NekoKitsune/NekoMimi/issues",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],                                      
-    python_requires='>=3.6',               
-    py_modules=["NekoMimi"],             
-    package_dir={'src':''},     
-    install_requires=['pyfiglet']                     
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
 )
