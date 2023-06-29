747ac8922f2396f7a6ed2f8efd996e095b61ead5
NekoMimiOfficial <nekomimiofficial@proton.me>
2023-06-30 00:21:55 +0300

    more cleaning and automation with templates

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-pip3 install build wheel
-python3 -m build -n
+
+$version = "1.1.0"
+
+rm dist/*
 pip3 uninstall -y NekoMimi
+
+python3 buildTemplates.py $version
+#pip3 install build wheel
+python3 -m build -n
+#pip3 uninstall -y NekoMimi
 pip3 install dist/*.whl
+
+rm ./setup.py
+rm ./NekoMimi/__init__.py
