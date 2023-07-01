7cd9ca2082513a9d6ffbda54dee3fa7b09b4c181
NekoMimiOfficial <nekomimiofficial@proton.me>
2023-07-01 18:58:28 +0300

    implemented template builder

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-$version = "1.1.0"
+version='1.1.0'
 
 rm dist/*
-pip3 uninstall -y NekoMimi
+# pip3 uninstall -y NekoMimi
 
 python3 buildTemplates.py $version
 #pip3 install build wheel
 python3 -m build -n
-#pip3 uninstall -y NekoMimi
+pip3 uninstall -y NekoMimi
 pip3 install dist/*.whl
 
 rm ./setup.py
