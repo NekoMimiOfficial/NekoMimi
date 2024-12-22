a58d7aefd9ed00379835efe0c5c69deb71a6aa9a
NekoMimiOfficial <nekomimi@tilde.team>
2024-12-22 20:17:32 +0300

    added gitignore

---
+++ build/lib/templates/__init__.py
@@ -0,0 +1,14 @@
+"""
+NekoMimi is a python module created by NekoMimi
+which aims to make python development easier
+and less tiresome
+
+this module contains a variety of functions
+and tools that you'd find yourself using a lot 
+in most projects, so no more of that copy/paste
+action and more coding fluently
+"""
+
+__version__ = "<PLACEHOLDER>"
+__author__ = "NekoMimi"
+__repo__ = "https://github.com/NekoMimiOfficial/NekoMimi.git"
---
+++ build/lib/templates/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="NekoMimi",
+    version="<PLACEHOLDER>",
+    author="NekoMimi",
+    author_email="nekomimi@tilde.team",
+    description="A module to assist coding in python with useful shortcuts and tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NekoMimiOfficial/NekoMimi",
+    project_urls={
+        "Bug Tracker": "https://github.com/NekoMimiOfficial/NekoMimi/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"./": "NekoMimi/"},
+    install_requires = ['pyfiglet', 'requests', 'BeautifulSoup4'],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
