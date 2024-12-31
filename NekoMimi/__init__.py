d80c0be6cbc66af368868c4e7b8adbbea3a13617
NekoMimiOfficial <nekomimi@tilde.team>
2024-12-31 21:32:35 +0300

    v1.1.1 b0x010001 Bobbified the install

---
+++ NekoMimi/__init__.py
@@ -0,0 +1,35 @@
+"""
+NekoMimi is a python module created by NekoMimi
+which aims to make python development easier
+and less tiresome
+
+this module contains a variety of functions
+and tools that you'd find yourself using a lot 
+in most projects, so no more of that copy/paste
+action and more coding fluently
+"""
+
+##[Bob args]######################
+__title__ = 'NekoMimi'
+__author__ = "NekoMimiOffical"
+__author_email__ = "nekomimi@tilde.team"
+__description__ = "Multi-Tool of macros to facilitate development"
+__repository__ = "https://github.com/NekoMimiOffical/NekoMimi"
+__bug_tracker__ = "https://github.com/NekoMimiOffical/NekoMimi/issues"
+__license__ = "Apache2"
+__pyver__ = ">=3.11"
+__target__ = "OS Independent"
+__requirements__ = ['aiohttp']
+##################################
+
+__version__ = '1.1.1'
+__build__ = 0x010001
+
+"""
+Build number policy:
+~~~~~~~~~~~~~~~~~~~~
+
+The build number is divided into 2 sections
+-> the first 2 Hex digits representing a major code
+-> the last 4 Hex digits representing a fix code
+"""
--- build.sh
+++ build.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-version='1.1.1'
-
 rm dist/*
-# pip3 uninstall -y NekoMimi
 
-python3 buildTemplates.py $version
-#pip3 install build wheel
-python3 setup.py bdist_wheel
+bob
 pip3 uninstall -y NekoMimi --break-system-packages
 pip3 install dist/*.whl --break-system-packages
-
-rm ./setup.py
-rm ./NekoMimi/__init__.py
